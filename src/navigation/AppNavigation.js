5d1352714aa0e391cf92b181abcadcb0dbbce6fa
karan mittal <mittal.karan60@gmail.com>
2019-12-20 01:40:25 +0530

    first commit

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import {View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Button } from 'react-native';
 import {createStackNavigator} from "react-navigation-stack";
 import {createAppContainer} from "react-navigation";
 
-const HomeScreen = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {/*<View>*/}
-            <Text>HomeScreen</Text>
-        </View>
-    );
+class HomeScreen extends Component{
+    render(){
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                {/*<View>*/}
+                <Text>HomeScreen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details', {hell: '123456789'})}
+                />
+            </View>
+        );
+    }
 };
-const DetailsScreen = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            {/*<View>*/}
-            <Text>DetailsScreen</Text>
-        </View>
-    );
+class DetailsScreen extends Component{
+    render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        console.log("====>",this.props.navigation.getParam('hell'));
+        console.log("====>",this.props.navigation.state.params.hell);
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                {/*<View>*/}
+                <Text>DetailsScreen</Text>
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details', {hell: 'from details'})}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
+                <Button
+                    title="Go to fist screen"
+                    onPress={() => this.props.navigation.popToTop()}
+                />
+            </View>
+        );
+    }
 };
 
 const AppNavigator = createStackNavigator(
